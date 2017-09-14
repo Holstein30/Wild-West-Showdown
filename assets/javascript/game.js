3fe4ad1d7d7e2057e2d7e80d1beca010af6f5621
Dale Holstein <DaleHolstein@wifi_stu-10-38-89-34.uncc.edu>
2017-09-14 17:58:59 -0400

    mostly working - reset not working and some balancing problems

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,220 +1,398 @@
+$(document).ready(function start() {
+$(".w-l").hide();
+$("#alert").hide();
+$("#info").text("Choose a Cowboy!")
 // Variables
 
-clintPick = false;
-johnPick = false;
-leePick = false;
-samPick = false;
-wins = 0;
-losses = 0;
+$("#clint").show();
+$("#john").show();
+$("#lee").show();
+$("#sam").show();
 
-// Set character stats (HP, Power)
+var gameStart = false;
+var gameOver = false;
+var user = false;
+var userDead = false;
+var opponent = false;
+var opponent2 = false;
+var opponentsLeft = 3;
+var wins = 0;
+var losses = 0;
 
-let clint = {
-	health: 80,
-	power: 150
+// Character objects with stats (maybe add critical hit)
 
+// Clint
+
+var clint = {
+	name: "'Blondie'",
+	hp: 100,
+	attack: Math.floor(Math.random() * 100)
 }
 
-let john = {
-	health: 120,
-	power: 30
+// John
+
+var john = {
+	name: "'The Duke'",
+	hp: 200,
+	attack: Math.floor(Math.random() * 50)
 }
 
-let lee = {
-	health: 90,
-	power: 40
+// Lee
+
+var lee = {
+	name: "'Angel Eyes'",
+	hp: 150,
+	attack: Math.floor(Math.random() * 75)
 }
 
-let sam = {
-	health: 180,
-	power: 20
+// Sam
+
+var sam = {
+	name: "'The Stranger'",
+	hp: 250,
+	attack: Math.floor(Math.random() * 25)
 }
+// Sounds to be added if everything else works
 
-// Create attack object or function (Maybe counter attack too?)
+var clintSound;
+var johnSound;
+var leeSound;
+var samSound;
+var gunShot;
+var winSound;
+var loseSound;
+var backgroundMusic;
 
-function attack () {
-	clintAttack = Math.floor(Math.random() * clint.power);
-	johnAttack = Math.floor(Math.random() * john.power);
-	leeAttack = Math.floor(Math.random() * lee.power);
-	samAttack = Math.floor(Math.random() * sam.power);
-}
-
-// let attack = {
-// 	clintAttack: Math.floor(Math.random() * clint.power),
-// 	johnAttack: Math.floor(Math.random() * john.power),
-// 	leeAttack: Math.floor(Math.random() * lee.power),
-// 	samAttack: Math.floor(Math.random() * sam.power)
-// }
-
-// Set up click events to choose character
-
-$("#clint").on("click", function() {
-        $("#userPick").html("<img src='assets/images/Clint-Eastwood.jpg' alt='Clint Eastwood'>");
-        clintPick = true;
-        hideToggle();
-        opponents();
-});
-$("#john").on("click", function() {
-        $("#userPick").html("<img src='assets/images/John-Wayne.jpg' alt='John Wayne'>");
-        johnPick = true;
-        hideToggle();
-        opponents();
-});
-$("#lee").on("click", function() {
-        $("#userPick").html("<img src='assets/images/Lee-Van-Cleef.jpg' alt='Lee Van Cleef'>");
-        leePick = true;
-        hideToggle();
-        opponents();
-});
-$("#sam").on("click", function() {
-        $("#userPick").html("<img src='assets/images/Sam-Elliot.jpg' alt='Sam Elliot'>");
-        samPick = true;
-        hideToggle();
-        opponents();
-});
-
-// Toggle between title screen and duel screen
+// Hide toggle between title and duel screen
 
 function hideToggle () {
 	$('.hide').toggleClass('unhidden hidden');
 }
 
-// Make rest of characters the enemy
+// Character selection
 
-function opponents () {
-	if (clintPick) {
-		oppA = "john";
-		$("#opponentA").html("<img src='assets/images/John-Wayne.jpg' alt='John Wayne'>");
-		$("#opponentB").html("<img src='assets/images/Lee-Van-Cleef.jpg' alt='Lee Van Cleef'>");
-		$("#opponentC").html("<img src='assets/images/Sam-Elliot.jpg' alt='Sam Elliot'>");
-	}
-	else if (johnPick){
-		$("#opponentA").html("<img src='assets/images/Clint-Eastwood.jpg' alt='Clint Eastwood'>");
-		$("#opponentB").html("<img src='assets/images/Lee-Van-Cleef.jpg' alt='Lee Van Cleef'>");
-		$("#opponentC").html("<img src='assets/images/Sam-Elliot.jpg' alt='Sam Elliot'>");
-	}
-	else if (leePick) {
-		$("#opponentA").html("<img src='assets/images/John-Wayne.jpg' alt='John Wayne'>");
-		$("#opponentB").html("<img src='assets/images/Clint-Eastwood.jpg' alt='Clint Eastwood'>");
-		$("#opponentC").html("<img src='assets/images/Sam-Elliot.jpg' alt='Sam Elliot'>");		
+$(".chars").on("click", function () {
+	if (user == false) {
+		$(this).hide();
+		var newImg = $("<img>");
+		userImage = $(this).attr("src");
+		// $("#userPick").attr("src", userImage);
+		$("#userPick").append(newImg);
+		newImg.attr("src", userImage);
+		console.log(userImage);
+		userChar = $(this).attr("id");
+		user = true;
+		opponents();
 	}
 	else {
-		$("#opponentA").html("<img src='assets/images/John-Wayne.jpg' alt='John Wayne'>");
-		$("#opponentB").html("<img src='assets/images/Lee-Van-Cleef.jpg' alt='Lee Van Cleef'>");
-		$("#opponentC").html("<img src='assets/images/Clint-Eastwood.jpg' alt='Clint-Eastwood'>");
+		return;
 	}
-}
-
-// Draw event to attack
-
-$(document).ready(function() {
-      // Let user attack and have them immediately counter attacked by opponent
-      $("#DRAW").on("click", function() {
-      	if (clintPick) {
-      		if (oppA === "john") {
-      			attack();
-      			john.health -= clintAttack;
-      			clint.health -= johnAttack;
-      			console.log(john.health);
-      			console.log(clint.health);
-      			gameOver();
-      		}
-      		else if (oppA === "lee") {
-      			attack();
-      			lee.health -= clintAttack;
-      			clint.health -= leeAttack;
-      			console.log(lee.health);
-      			console.log(clint.health);
-      			gameOver();
-      		}
-      		else {
-      			attack();
-      			sam.health -= clintAttack;
-      			clint.health -= samAttack;
-      			console.log(sam.health);
-      			console.log(clint.health);
-      			gameOver();
-      		}
-      	}
-      });
-
+	
 });
 
 
-// If HP=0 that character loses. 
-// -if character is users character then player loses & replace picture with YOU LOSE
-// -if it is opponents then move to next opponent & Display options for next opponent
-// -if opponent out of characters then the user wins & replace opponent picutre with YOU WIN
-// -tally wins & losses
-// -change draw button to restart button
-
-function gameOver () {
-	if (clintPick && oppA === "john") {
-		if (clint.health <= 0) {
-			$("#userPick").html("YOU LOST");
-			console.log("YOU LOSE");
-			losses++;
-			$("#losses").html("Losses: "+ losses);
-			restart();
-		}
-		else if (john.health <= 0) {
-			oppA = "lee";
-			$("#opponentA").html("<img src='assets/images/Lee-Van-Cleef.jpg' alt='Lee Van Cleef'>");
-			$("#opponentB").html("<img src='assets/images/Sam-Elliot.jpg' alt='Sam Elliot'>");
-			$("#opponentC").html("Lost");
-
-			console.log("John lose");
-		}
-		else {
-			return;
-		}
-	}
-	else if (clintPick && oppA === "lee") {
-		if (clint.health <= 0) {
-			$("#userPick").html("YOU LOST");
-			console.log("YOU LOSE");
-			losses++;
-			$("#losses").html("Losses: "+ losses);
-			restart();
-		}
-		else if (lee.health <= 0) {
-			oppA = "sam";
-			$("#opponentA").html("<img src='assets/images/Sam-Elliot.jpg' alt='Sam Elliot'>");
-			$("#opponentB").html("LOST");
-			$("#opponentC").html("LOST");
-
-			console.log("LEE lose");
-		}
+function opponents () { 
+	if (opponent === false) {
+		$("#info").html("<h2 id='info'>Choose your Opponent<h2>")
+		$(".chars").on("click", function() {
+			$(this).hide();
+			$("#opponentPick img").remove();
+			opponent = true;
+			newImg = $("<img id='remove'>");
+			opponentImage = $(this).attr("src");
+			// $("#opponentPick").attr("src", opponentImage);
+			$("#opponentPick").append(newImg);
+			newImg.attr("src", opponentImage);
+			opponentChar = $(this).attr("id");
+			console.log(opponentImage);
+			console.log(opponentChar);
+			$("#info").html("<h2 id='info'>Press Draw to Attack</h2>");
+			hideToggle();
+		});
 	}
 	else {
-		if (clint.health <= 0) {
-			$("#userPick").html("YOU LOST");
-			console.log("YOU LOSE");
-			losses++;
-			$("#losses").html("Losses: " + losses);
-			restart();
-		}
-		else if (sam.health <= 0) {
-			$("#opponentA").html("YOU WIN!!!");
-			$("#opponentB").html("LOST");
-			$("#opponentC").html("LOST");
-
-			console.log("YOU WIN");
-			wins++;
-			$("#wins").html("Wins: " + wins);
-			restart();
-		}
-	}		
+		return;
+	}
 }
 
-function restart () {
-	$("#DRAW").html("Restart");
-	clint.health = 80;
-	john.health = 120;
-	lee.health = 90;
-	sam.health = 180;
-	$("#DRAW").on("click", function () {
-		$("#DRAW").html("DRAW");
-		hideToggle();
-	});
+// Determine which characters are currenlty fighting and set up draw event to attack
+
+$("#DRAW").on("click", function() {
+	if (user && opponent) {
+		if ((userChar === "clint" || opponentChar === "clint") && (userChar === "john" || opponentChar === "john")) {
+			console.log("CLINT v JOHN");
+			console.log("2" + opponentChar);
+			john.hp -= clint.attack;
+			clint.hp -= john.attack;
+			console.log(john.hp);
+			console.log(clint.hp);
+			check();
+			checkHealth();
+		}
+		else if ((userChar === "clint" || opponentChar === "clint") && (userChar === "lee" || opponentChar === "lee")) {
+			console.log("CLINT v LEE");
+			lee.hp -= clint.attack;
+			clint.hp -= lee.attack;
+			console.log(lee.hp);
+			console.log(clint.hp);
+			check();
+			checkHealth();
+		}
+		else if ((userChar === "clint" || opponentChar === "clint") && (userChar === "sam" || opponentChar === "sam")) {
+			console.log("CLINT v SAM");
+			sam.hp -= clint.attack;
+			clint.hp -= sam.attack;
+			console.log(sam.hp);
+			console.log(clint.hp);
+			check();
+			checkHealth();
+		}
+		else if ((userChar === "john" || opponentChar === "john") && (userChar === "lee" || opponentChar === "lee")) {
+			console.log("JOHN v LEE");
+			john.hp -= lee.attack;
+			lee.hp -= john.attack;
+			console.log(lee.hp);
+			console.log(john.hp);
+			check();
+			checkHealth();
+		}
+		else if ((userChar === "john" || opponentChar === "john") && (userChar === "sam" || opponentChar === "sam")) {
+			console.log("JOHN v SAM");
+			sam.hp -= john.attack;
+			john.hp -= sam.attack;
+			console.log(sam.hp);
+			console.log(john.hp);
+			check();
+			checkHealth();
+		}
+		else if ((userChar === "lee" || opponentChar === "lee") && (userChar === "sam" || opponentChar === "sam")) {
+			console.log("LEE v SAM");
+			sam.hp -= lee.attack;
+			lee.hp -= sam.attack;
+			console.log(sam.hp);
+			console.log(lee.hp);
+			check();
+			checkHealth();
+		}
+		else {
+			console.log("got to else");
+		}
+	}
+
+
+// Check if game is over
+
+function check () {
+	if (opponentsLeft === 0) {
+		$(".w-l").show();
+		wins++;
+		console.log("wins " + wins);
+		$("#wins").html("Wins: " + wins);
+		$("#info").html("YOU WON!!!");
+		gameOver();
+	}
+	else if (userDead) {
+		$(".w-l").show();
+		losses++;
+		$("#losses").html("Losses: " + losses);
+		$("#info").html("YOU LOST!")
+		$("#clint").hide();
+		$("#john").hide();
+		$("#lee").hide();
+		$("#sam").hide();
+		gameOver();
+	}
+	else {
+		return;
+	}
 }
+
+// Check health of each character if >= 0 then check if game is over or choose another opponent
+
+function checkHealth () {
+	if (userChar && (opponentsLeft >= 1)) {
+			if ((userChar === "clint" || opponentChar === "clint") && (userChar === "john" || opponentChar === "john")) {
+				if (clint.hp <= 0) {
+					if (userChar === "clint") {
+						userDead = true;
+						return;
+					}
+					opponentsLeft--;
+					check();
+					hideToggle();
+					console.log(opponent);
+					opponents();
+					console.log("Clint Lose" + opponentsLeft);
+				}
+				else if (john.hp <= 0) {
+					if (userChar === "john") {
+						userDead = true;
+					}
+					opponentsLeft--;
+					check();
+					hideToggle();
+					console.log(opponent);
+					opponents();
+					console.log("John lose" + opponentsLeft);
+				}
+			}
+			else if ((userChar === "clint" || opponentChar === "clint") && (userChar === "lee" || opponentChar === "lee")) {
+				if (clint.hp <= 0) {
+					if (userChar === "clint") {
+						userDead = true;
+					}
+					opponentsLeft--;
+					check();
+					hideToggle();
+					console.log(opponent);
+					opponents();
+					console.log("Clint Lose" + opponentsLeft);
+				}
+				else if (lee.hp <= 0) {
+					if (userChar === "lee") {
+						userDead = true;
+					}
+					opponentsLeft--;
+					check();
+					hideToggle();
+					console.log(opponent);
+					opponents();
+					console.log("Lee lose" + opponentsLeft);
+				}
+			}
+			else if ((userChar === "clint" || opponentChar === "clint") && (userChar === "sam" || opponentChar === "sam")) {
+				if (clint.hp <= 0) {
+					if (userChar === "clint") {
+						userDead = true;
+					}
+					opponentsLeft--;
+					check();
+					hideToggle();
+					console.log(opponent);
+					opponents();
+					console.log("Clint Lose" + opponentsLeft);
+				}
+				else if (sam.hp <= 0) {
+					if (userChar === "sam") {
+						userDead = true;
+					}
+					opponentsLeft--;
+					check();
+					hideToggle();
+					console.log(opponent);
+					opponents();
+					console.log("Sam lose" + opponentsLeft);
+				}
+			}
+			else if ((userChar === "john" || opponentChar === "john") && (userChar === "lee" || opponentChar === "lee")) {
+				if (john.hp <= 0) {
+					if (userChar === "john") {
+						userDead = true;
+					}
+					opponentsLeft--;
+					check();
+					hideToggle();
+					console.log(opponent);
+					opponents();
+					console.log("JOHN Lose" + opponentsLeft);
+				}
+				else if (lee.hp <= 0) {
+					if (userChar === "lee") {
+						userDead = true;
+					}
+					opponentsLeft--;
+					check();
+					hideToggle();
+					console.log(opponent);
+					opponents();
+					console.log("LEE lose" + opponentsLeft);
+				}
+			}
+			else if ((userChar === "john" || opponentChar === "john") && (userChar === "sam" || opponentChar === "sam")) {
+				if (john.hp <= 0) {
+					if (userChar === "john") {
+						userDead = true;
+					}
+					opponentsLeft--;
+					check();
+					hideToggle();
+					console.log(opponent);
+					opponents();
+					console.log("John Lose" + opponentsLeft);
+				}
+				else if (sam.hp <= 0) {
+					if (userChar === "sam") {
+						userDead = true;
+					}
+					opponentsLeft--;
+					check();
+					hideToggle();
+					console.log(opponent);
+					opponents();
+					console.log("Sam lose" + opponentsLeft);
+				}
+			}
+			else if ((userChar === "lee" || opponentChar === "lee") && (userChar === "sam" || opponentChar === "sam")) {
+				if (lee.hp <= 0) {
+					if (userChar === "lee") {
+						userDead = true;
+					}
+					opponentsLeft--;
+					check();
+					hideToggle();
+					console.log(opponent);
+					opponents();
+					console.log("LEE Lose" + opponentsLeft);
+				}
+				else if (sam.hp <= 0) {
+					if (userChar === "sam") {
+						userDead = true;
+					}
+					opponentsLeft--;
+					check();
+					hideToggle();
+					console.log(opponent);
+					opponents();
+					console.log("Sam lose" + opponentsLeft);
+				}
+			}
+			else {
+				console.log("ELLSLELSLSELELSSEEE");
+				return;
+			}		
+		}
+	}
+
+
+function gameOver () {
+	// $("#DRAW").html("Restart");
+	clint.hp = 80;
+	john.hp = 120;
+	lee.hp = 90;
+	sam.hp = 180;
+	// $("#DRAW").on("click", function () {
+	// 	$("#DRAW").html("DRAW");
+	// 	start();
+	// });
+	$("#alert").show();
+	document.body.addEventListener('keypress', start);
+	}
+});
+
+});
+
+// Whats left to be added
+// -------------
+// MOST IMPORTANT: Display characters names and health
+// Play sounds when choosing characters/hitting draw/win/lose/restart/etc.
+
+
+// Known Bugs
+// -----------
+// When player loses screen doesn't toggle and everytime you hit draw losses increases
+// When player hits a key to restart game and then chooses a character 3 characters show on duel screen
+// 
+
+
+
+
